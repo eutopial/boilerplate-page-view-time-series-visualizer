62134a0c694c9b64231540b52b77e9c607539c9e
Paine plez <paineplez@gmail.com>
2025-04-02 21:52:09 +0000

    #

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -60,8 +60,7 @@ def draw_box_plot():
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
     df_box["value"] = df_box["value"].astype(int)
-    df_box["month"] = df_box["month"].astype("category")
-    df_box["year"] = df_box["year"].astype("category") 
+    df_box["month_angka"] = df_box["date"].dt.month
 
     df_box = df_box.sort_values('month')
     # Draw box plots (using Seaborn)
